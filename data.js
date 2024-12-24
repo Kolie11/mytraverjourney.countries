a259cb25b9778e57f3ebda9e76321dd3072eb618
Fassou Mathias Kolie <151475303+Kolie11@users.noreply.github.com>
2024-12-24 16:49:01 +0100

    TravelJourney with React
    TravelJourney with React

---
+++ data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://imgs.search.brave.com/azg5wTIJMZkywvw11rA9_PpDhErJKLryv2NrxEGIFz0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZi5i/c3RhdGljLmNvbS94/ZGF0YS9pbWFnZXMv/aG90ZWwvc3F1YXJl/NjAwLzI0NzE4ODg4/OS53ZWJwP2s9NTY4/NThiNmFlNTYwZDZh/N2NiMWI4M2JhZjli/ODY4OGFjNzhhN2I5/MzIyNDY0ZTcwNzg5/MDRlMDRmOTM5ZWE2/YiZvPQ",
+            alt: "Mount Fuji"
+        },
+        title: "Kolding Hotel",
+        country: "Danemark",
+        googleMapsLink: "https://satellites.pro/Google_plan/Denmark_map",
+        dates: "28 Nov, 2024 - 30 Nov, 2024",
+        text: "Hotel Koldingfjord in Denmark is a historic hotel and conference center surrounded by scenic natural beauty. Overlooking Kolding Fjord, it combines elegant architecture, luxurious accommodations, and modern amenities, making it ideal for relaxing getaways, events, and inspiring meetings."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGFyaXMlMjBFZmZlbCUyMFRvd2VyfGVufDB8fDB8fHww",
+            alt: "Effel tower"
+        },
+        title: "Eiffel Tower",
+        country: "France",
+        googleMapsLink: "https://satellites.pro/Paris_map.France#google_vignette",
+        dates: "23 Aug, 2023 - 5 Sep, 2023",
+        text: "The Eiffel Tower, an iconic Parisian landmark, stands 330 meters tall in France. Built in 1889, it features intricate iron latticework, stunning panoramic views, and a legacy as a global cultural and architectural symbol, attracting millions of visitors annually."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://imgs.search.brave.com/wRl1-kadYHFkcjSQXoSKUSnAJfnTGgbB7Mql5MWBzHM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hdmll/d2Zyb21teXNlYXQu/ZGUvbWVkaXVtL2dv/bG9za3ktMjAxOTEw/MjExODAwMTIuanBn",
+            alt: "Udina Signal Park"
+        },
+        title: "Udina Signal Park",
+        country: "Germany",
+        googleMapsLink: "https://www.bahnhof.de/dortmund-signal-iduna-park/karte",
+        dates: "13 Dec 2023",
+        text: "As a fan of Dortmund, I went to watch the UEFA Champions League match between Dortmund and Paris Saint-Germain. It was amazing!"
+    },
+]
